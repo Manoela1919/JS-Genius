bba5a8588c94de9bc3a844ae4d91193f66c1a31c
Manoela Sanches <plmqazmanu@gmail.com>
2024-02-03 02:21:34 -0300

    Add counter and responsivity

--- script.js
+++ script.js
@@ -1,108 +1,115 @@
 let sequence = [];
-let userAttempt = []
-let gameStatus = 'over'
-let level = 0
-let historic = []
+let userAttempt = [];
+let gameStatus = "over";
+let level = 0;
+let historic = [];
 
-let localHistoric = localStorage.getItem('historic')
+let localHistoric = localStorage.getItem("historic");
 if (localHistoric) {
-  historic = JSON.parse(localHistoric)
+  historic = JSON.parse(localHistoric);
 }
-setMenu()
+setMenu();
 
 // Função que da inicio ao jogo
 function start() {
-  level = 0
-  sequence = []
-  userAttempt = []
-  document.getElementById('menu').style.display = 'none'
-  nextLevel()
+  level = 0;
+  sequence = [];
+  userAttempt = [];
+  document.getElementById("menu").style.display = "none";
+  document.getElementById("counter").style.display = "block";
+  nextLevel();
 }
 
 // Função que adiciona mais um item a sequência e mostra a ordem
 function nextLevel() {
-  userAttempt = []
-  level++
+  userAttempt = [];
+  level++;
+  let counter = document.getElementById("counter");
+  counter.innerHTML = level;
   let newItem = Math.floor(Math.random() * 4);
   let buttons = document.getElementById("buttons-wrapper");
   sequence.push(newItem);
-  document.activeElement.blur()
+  document.activeElement.blur();
   setTimeout(() => {
+    let speed = 50 * level;
+    if (speed > 600) {
+      speed = 600;
+    }
     for (let index = 0; index < sequence.length; index++) {
       setTimeout(() => {
         buttons.children[sequence[index]].focus();
         setTimeout(() => {
           buttons.children[sequence[index]].blur();
-        }, 700);
-      }, 800 * index);
+        }, 700 - speed);
+      }, 800 * index - speed);
     }
-  }, 250);
+  }, 800);
 }
 
 // Função que valida se o usuário clicou na ordem certa
 function pressed(pressValue) {
-  userAttempt.push(pressValue)
-  let slicedSequence = sequence.slice(0, userAttempt.length)
+  userAttempt.push(pressValue);
+  let slicedSequence = sequence.slice(0, userAttempt.length);
   if (JSON.stringify(userAttempt) !== JSON.stringify(slicedSequence)) {
-    gameOver()
-    return
+    gameOver();
+    return;
   }
   if (slicedSequence.length === sequence.length) {
-    nextLevel()
+    nextLevel();
   }
 }
 
 // Função ativada quando o usuário erra a sequência
 function gameOver() {
-  gameStatus = 'over'
-  if (historic.length < 10 || historic[historic.length -1].score < level) {
-    setHistoric()
+  gameStatus = "over";
+  document.getElementById("counter").style.display = "none";
+  if (historic.length < 10 || historic[historic.length - 1].score < level) {
+    setHistoric();
   }
-  let menuElement = document.getElementById('menu')
-  menuElement.style.display = 'grid'
-  menuElement.children[0].innerHTML = `Game Over! Score: ${level}`
-}
+  let menuElement = document.getElementById("menu");
 
-function showMenu(isGameOver) {
-  let historic = localStorage.getItem('historic')
+  menuElement.style.display = "grid";
+  menuElement.children[0].innerHTML = `Game Over! Score: ${level}`;
 }
 
 // Função que atualiza o histórico no local storage
 function setHistoric() {
   if (historic.length === 10) {
-    historic.pop
+    historic.pop;
   }
 
   const today = new Date();
   const yyyy = today.getFullYear();
-  let mm = today.getMonth() + 1; 
+  let mm = today.getMonth() + 1;
   let dd = today.getDate();
   if (dd < 10) {
-    dd = '0' + dd;
+    dd = "0" + dd;
   }
   if (mm < 10) {
-    mm = '0' + mm;
+    mm = "0" + mm;
   }
-  const formattedToday = dd + '/' + mm + '/' + yyyy;
+  const formattedToday = dd + "/" + mm + "/" + yyyy;
 
   let newItem = {
     score: level,
-    date: formattedToday
-  }
+    date: formattedToday,
+  };
 
-  historic.push(newItem)
+  historic.push(newItem);
   historic.sort((a, b) => b.score - a.score);
 
-  localStorage.setItem('historic', JSON.stringify(historic))
-  setMenu()
+  localStorage.setItem("historic", JSON.stringify(historic));
+  setMenu();
 }
 
 // Coloca os dados do histórico no HTML
 function setMenu() {
-  let scores = document.getElementById('menu').children[1]
+  let scores = document.getElementById("menu").children[1];
   for (let index = 0; index < historic.length; index++) {
     const element = historic[index];
-    scores.children[index].children[0].innerHTML = element.score
-    scores.children[index].children[1].innerHTML = element.date
+    scores.children[index].children[0].innerHTML = `${index + 1}- ${
+      element.score
+    } pontos`;
+    scores.children[index].children[1].innerHTML = element.date;
   }
 }
